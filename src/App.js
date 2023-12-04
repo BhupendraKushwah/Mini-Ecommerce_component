4c796424aa5b5b0e9d57962df447846223023ea8
Bhupendra Kushwah <bhupendrakushwah977@gmail.com>
2023-12-04 21:45:35 +0530

    day 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Product from './Component/Product';
+import Home from './Component/Home';
 
 function App() {
   return (
     <div className="App">
-      <Product/>
+      <Home/>
     </div>
   );
 }
---
+++ src/Component/Home.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import Product from './Product'
+
+const Home = () => {
+    const [product,setProduct]=useState([])
+    useEffect(()=>{
+        const fetchProducts=async()=>{
+        const response=await fetch("http://localhost:5000/Home")
+        const result= await response.json()
+        setProduct(result)
+    }
+    fetchProducts()
+    },[])
+  return (
+   <div className="container">
+    {product.map(item=>{
+        return <Product key={item._id} item={item}/>
+    })
+    }
+    </div>
+  )
+}
+
+export default Home
